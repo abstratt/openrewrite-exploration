878a6698a6ec35d995521bec3d344595132af04e
Rafael Chaves <rchaves@gradle.com>
2025-02-05 14:28:04 -0300

    Initial

--- gradle-migrations/src/main/kotlin/org/gradle/migration/ImportAssign.kt
+++ gradle-migrations/src/main/kotlin/org/gradle/migration/ImportAssign.kt
@@ -6,12 +6,12 @@ import org.openrewrite.java.tree.J
 import org.openrewrite.java.tree.JavaType
 import org.openrewrite.kotlin.KotlinIsoVisitor
 
+@Suppress("unused")
 class ImportAssign : Recipe() {
     override fun getDisplayName(): String = "Add assign import to Kotlin files"
     override fun getDescription(): String = "Adds `import org.example.utils.assign` to Kotlin files that reference `setMyProperty`."
 
     override fun getVisitor(): KotlinIsoVisitor<ExecutionContext> {
-        println("getVisitor() ${this}")
         return object : KotlinIsoVisitor<ExecutionContext>() {
             override fun visitAssignment(assignment: J.Assignment, p: ExecutionContext): J.Assignment {
                 val r = super.visitAssignment(assignment, p)
