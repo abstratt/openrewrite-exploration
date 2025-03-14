407439ef61f554a7a8b9a0997c95620352e23da4
Rafael Chaves <rchaves@gradle.com>
2025-03-14 16:51:37 -0300

    Clean up init script

--- gradle-migrations/apply-migrations.gradle.kts
+++ gradle-migrations/apply-migrations.gradle.kts
@@ -1,35 +1,25 @@
 val pluginJar = file("build/libs/gradle-migrations.jar")
 initscript {
-//    val pluginJar = file("build/libs/gradle-migrations.jar")
     repositories {
-//        mavenCentral()
         gradlePluginPortal()
     }
     dependencies {
-//        classpath(files(pluginJar))
-//        classpath("org.openrewrite:rewrite-core:8.45.1")
-//        classpath("org.openrewrite:rewrite-kotlin:1.27.0")
-//        classpath("org.openrewrite:rewrite-java:8.45.1")
-//        classpath("org.openrewrite.meta:rewrite-analysis:2.17.0")
-//        classpath("org.openrewrite.meta:rewrite-analysis:2.17.0")
-        classpath("org.openrewrite.rewrite:org.openrewrite.rewrite.gradle.plugin:7.1.4")
+        classpath("org.openrewrite:plugin:latest.release")
     }
-//    if (!pluginJar.exists()) {
-//        println("Plugin JAR not found at $pluginJar")
-//    }
 }
 
-//beforeSettings {
-//    apply<org.gradle.migration.plugin.MigrationPlugin>()
-//}
 
 rootProject {
     apply<org.openrewrite.gradle.RewritePlugin>()
     require(pluginJar.exists()) {
         println("$pluginJar not found")
     }
-    repositories {
-        mavenCentral()
+    afterEvaluate {
+        if (repositories.isEmpty()) {
+            repositories {
+                mavenCentral()
+            }
+        }
     }
     dependencies {
         "rewrite"(files(pluginJar))
@@ -37,16 +27,13 @@ rootProject {
     project.extensions.findByType(org.openrewrite.gradle.RewriteExtension::class.java)?.let { rewrite ->
         // Configure the extension
         rewrite.activeRecipe(
+            "org.openrewrite.FindParseFailures",
             "org.gradle.migration.Gradle8to9"
         )
         rewrite.setExportDatatables(true)
     }
 }
 
-settingsEvaluated {
-    println("Plugins")
-    (pluginManager as org.gradle.api.internal.plugins.PluginManagerInternal).pluginContainer.forEach {
-        println(it)
-    }
-}
+
+
 
